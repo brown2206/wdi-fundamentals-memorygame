2b2a3c1398fc8032fba7e26afdd0180c31fc7613
brown2206 <brown.angelo.jr@gmail.com>
2016-09-03 00:40:09 -0400

    manipulated the DOM

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-if (cardOne === cardTwo) {
+var newBoard = document.getElementById("game-board");
+function createBoard (newBoard) {
+for (i = 0; i < 4; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = "card";
+    newBoard.appendChild (newCard);
+}
+}
+createBoard (newBoard);
+/*if (cardOne === cardTwo) {
     alert ("You found a match!");
 } else {
     alert ("Sorry, try again.");
@@ -21,4 +30,4 @@ if (cardOne === CardThree) {
     alert ("You found a match!");
 } else {
     alert ("Sorry, try again.");
-}
+}*/
