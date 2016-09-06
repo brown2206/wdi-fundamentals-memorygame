2a1092cbd39a2e75de8cf5035138631b5824d21d
brown2206 <brown.angelo.jr@gmail.com>
2016-09-05 20:56:54 -0400

    completed WDI memorygame

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 var newBoard = document.getElementById("game-board");
 function createBoard (newBoard) {
-for (i = 0; i < 4; i++) {
+for (i = 0; i < cards.length; i++) {
     var newCard = document.createElement('div');
     newCard.className = "card";
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
     newBoard.appendChild (newCard);
 }
+}
+
+function isTwoCards(cards) {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card')=== "king") {
+    this.innerHTML = '<img src="king-37155_1280.png" />';
+  }
+  else {
+    this.innerHTML = '<img src="queen-37156_1280.png" />';
+  }
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch() {
+  if (cardsInPlay[0]===cardsInPlay[1]) {
+    alert ("You found a match!");
+  } else {
+    alert ("Sorry, try again.");
+  }
 }
 createBoard (newBoard);
 /*if (cardOne === cardTwo) {
